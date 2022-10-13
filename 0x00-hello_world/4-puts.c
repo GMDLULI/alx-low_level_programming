6064df6f7a2b2d40e6cd9f00c63032283ca3b832
GMDLULI <gugulethumdluli98@gmail.com>
2022-10-13 13:43:45 -0700

    my first low level code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main-Serves as the entry point for the program
  *
- * Return: By defualt returns zero(0) if no erros otherwise returns non-zero value.
+ * Return: returns zero(0) if no erros otherwise returns non-zero value.
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-}	
+}
