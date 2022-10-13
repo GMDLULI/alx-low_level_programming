bfabd3dd2503d9832cfb76476fc6d217f3184933
GMDLULI <gugulethumdluli98@gmail.com>
2022-10-13 13:53:34 -0700

    use printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+/**
+ * main-entry point for the program
+ *
+ * Return:return zero(0) value if no errors otherwise return non-zero value
+ */
+#include <stdio.h>
+int main(void)
+{
+	printf("\"with proper grammer, but the outcome is a piece of art,");
+	return (0);
+}
