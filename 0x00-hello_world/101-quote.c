ff9d8f7f295c37474299da0460dfb6c29dc5472f
GMDLULI <gugulethumdluli98@gmail.com>
2022-10-14 04:55:26 -0700

    Create program without using printf or puts

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+ * main-Entry point for the prgram
+ *
+ * return: Return value one (1) if no errors, otherwise return zreo (0)
+ */
+int main(void)
+{
+	write(2,"and that piece of art is useful\" -Dora Korpar, 2015-10-19\n",59);
+	return(1);
+}
