04290726f097022df20e762b6e2aed07818b1841
GMDLULI <gugulethumdluli98@gmail.com>
2022-10-14 04:23:21 -0700

    show size of data types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main-entry point of program
+ *
+ * return:return zero(0) if no erros and non-zero otherwise
+ */
+int main(void)
+{
+	int a;
+	long int b;
+	long long int  c;
+	char  d;
+	float f;
+
+	printf("Size of int is: %lu byte(s)\n" , (unsigned long)sizeof('a'));
+	printf("Size of long int is: %lu byte(s)\n" , (unsigned long)sizeof('b'));
+	printf("Size of long long int is: %lu byte(s)\n" ,(unsigned long)sizeof('c'));
+	printf("Size of char is: %lu byte(s)\n" ,(unsigned long)sizeof('d'));
+	printf("Size of float is: %lu byte(s)\n" , (unsigned long)sizeof('f'));
+
+	return(0);
+}
