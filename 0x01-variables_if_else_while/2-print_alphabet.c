766c7cd6a16077c56f2a7f6b89abfdba342a5926
GMDLULI <gugulethumdluli98@gmail.com>
2022-10-15 10:38:48 -0700

    print lower case alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main- entry point of program 
+ *
+ * return: return (0) value if no errors, otherwise return non-zero
+ */
+
+int main(void)
+{
+	char i;
+	for(i = 'a'; i < 'z'; i++);
+	{
+	putchar(i);
+	}
+	putchar('\n');
+	return(0);
+}
