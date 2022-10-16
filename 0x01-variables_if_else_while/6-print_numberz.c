02d4ab835f36732f905bc62a7544b50a4a32e2b9
GMDLULI <gugulethumdluli98@gmail.com>
2022-10-15 22:16:12 -0700

    print base 10 numbers using putchar function

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main- entry point of program
+ *
+ * Return:0 if no errors, return non-zero if errors
+ */
+int main(void)
+{
+	int i;
+	for(i = '0';i <= '9'; i++);
+	{
+	putchar(i);
+	}
+	putchar('\n');
+	return(0);
+}
