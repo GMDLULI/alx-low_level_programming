b725a2d366d37ef077d4201674a8fae10bdcfb02
GMDLULI <gugulethumdluli98@gmail.com>
2022-10-15 22:24:51 -0700

    print alphabets in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main- entry point of program
+ *
+ * return:0 if no errors return non-zero if errors
+ */
+
+int main(void)
+{
+	int i;
+	for(i = 'z'; i < 'a'; i--);
+	{
+	putchar(i);
+	putchar('\n');
+	}
+	return(0);
+}
