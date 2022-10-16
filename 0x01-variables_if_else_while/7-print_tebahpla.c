7324c80292f08c58050773c1fc6c3863ec6f500b
GMDLULI <gugulethumdluli98@gmail.com>
2022-10-16 11:59:38 -0700

    print lowercase alphabet in reverse

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 /**
- * main-program entry point.
+ * main-program entry point
  *
- * return:0 if no errors,  non-zero if errors
+ * return:0 if no errors, non-zero if errors
  */
 
 int main(void)
