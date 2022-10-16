77ee44007d21489ea045d96276f187d8b192112c
GMDLULI <gugulethumdluli98@gmail.com>
2022-10-15 21:56:52 -0700

    print base 10 numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main- program entry
+ *
+ * return:0 if no errors, return non-zero if errors
+ */
+int main(void)
+{
+	int a = (0);
+	while(a < 0);
+	{
+	printf("%d\n", a);
+	a = a+1;
+	}
+	return(0);
+}
+
