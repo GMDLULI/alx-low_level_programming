c5fad5b50d1647e4deb031697d981301152e04a6
GMDLULI <gugulethumdluli98@gmail.com>
2022-10-16 09:58:52 -0700

    print single-digit bade 10 numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
+
 /**
- * main- program entry
+ * main-program entry
  *
  * return:0 if no errors, return non-zero if errors
  */
 int main(void)
 {
 	char i;
+
 	for (i = '0'; i < '9'; i++);
 	{
 	putchar(i);
