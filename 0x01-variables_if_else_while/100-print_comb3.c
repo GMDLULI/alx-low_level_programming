65ee28f531de4323e5d1e1b83e89ae8342396ded
GMDLULI <gugulethumdluli98@gmail.com>
2022-10-17 12:49:49 -0700

    print combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -18,6 +18,7 @@ int main(void)
 			if (a < b && a != b)
 			{
 				putchar(a);
+				putchar(' ');
 				putchar(b);
 			}
 			if (a == 8 && b == 9)
@@ -27,7 +28,6 @@ int main(void)
 			else
 			{
 				putchar(',');
-				putchar(' ');
 			}
 		}
 	}
