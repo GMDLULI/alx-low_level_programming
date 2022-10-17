afdf472ba083ac5c20d9aef2f40e3a07f215b725
GMDLULI <gugulethumdluli98@gmail.com>
2022-10-17 11:46:56 -0700

    print combinations of two-two digit

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -15,13 +15,13 @@ int main(void)
 	{
 		for (b = a + 1; b <= 99; a++)
 		{
-			putchar('0' + a / 10);
-			putchar('0' + a % 10);
+			putchar('0' + a /10);
+			putchar('0' + a %10);
 
 			putchar(' ');
 
-			putchar('0' + b / 10);
-			putchar('0' + b % 10);
+			putchar('0' + b /10);
+			putchar('0' + b %10);
 		}
 		if (a == 98 && b == 99)
 		{
