486dc06419b094c90e93887ae83c6fe30c374f88
GMDLULI <gugulethumdluli98@gmail.com>
2022-10-19 12:35:31 -0700

    print all natural numbers

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_to_98- prints all natural numbers from n to 98
+ * print_to_98- prints all natural numbers
  * @n: numbers printed
  * return:Always 0
  */
@@ -9,10 +9,13 @@ void print_to_98(int n)
 
 	for (n = 0; n <= 98; n++)
 	{
-		_putchar(n);
+		_putchar(n'\n');
+	}
+	for(n > 98; n--);
+	{
+		_putchar(n'\n');
 		_putchar(' ');
 		_putchar(',');
 	}
-	_putchar('\n');;
 }
 
