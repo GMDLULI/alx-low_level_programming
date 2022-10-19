3c803a21b9d42733c18235540310508dc8038909
GMDLULI <gugulethumdluli98@gmail.com>
2022-10-18 18:04:09 -0700

    checks alphabetic character

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,16 +2,20 @@
 /**
  * _isalpha-checks for alphabet charecter
  * @c:character to check lowercase or uppercase
- * return: return 0 if character is lowercase or uppercase, return 1 if otherwise
+ * return:return 0-if character is lowercase or uppercase, 1-if otherwise
  */
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	if (c >= 'a' && c <= 'z')
 	{
-		return (0);
+		return (1);
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
 	}
 	else
 	{
-		return (1);
+		return (0);
 	}
 }
