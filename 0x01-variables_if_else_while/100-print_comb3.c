99b8c26af0f2d939348dcfd84ef9dfcefaff4bd1
GMDLULI <gugulethumdluli98@gmail.com>
2022-10-19 02:31:13 -0700

    print combinations of two digits

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,32 +10,26 @@ int main(void)
 {
 	int a;
 	int b;
+	int c;
 
 	for (a = '0'; a <= '9'; a++)
 	{
 		for (b = '0'; b <= '9'; b++)
 		{
+			for (c = '0')
+
 			if (a < b && a != b)
 			{
 				putchar(a);
 				putchar(b);
-			}
-			if (a == b)
-			{
-				continue;
-			}
-			if (a == 8 && b == 9)
-			{
-				break;
-			}
-			else
-			{
-				putchar(' ');
-				putchar(',');
+				if (a == 8 && b == 9)
+				{
+					break;
+				}
 			}
 		}
-		}
+		putchar(' ');
+		putchar(',');
 	}
-	putchar('\n');
 	return (0);
 }
