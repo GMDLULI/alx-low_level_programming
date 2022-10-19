1c77d0fdfea7e321bed741f45a7221c1bf4c7f77
GMDLULI <gugulethumdluli98@gmail.com>
2022-10-19 03:37:56 -0700

    ,print absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _abs-prints absolute value of integer
+ * @a:integer to compute its absolute value
+ * return:return the absolute values
+ */
+int _abs(int)
+{
+	int r;
+	int absolute value = k
+
+		if (r < 0)
+		{
+			_putchar(k* ,'-1');
+		}
+		else
+		{
+			return (k);
+		}
+}
