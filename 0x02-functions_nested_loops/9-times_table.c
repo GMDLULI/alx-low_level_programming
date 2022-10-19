cdf212c3aaa6cb7353b21b2a5ae375d3f9d387e9
GMDLULI <gugulethumdluli98@gmail.com>
2022-10-19 15:37:34 -0700

    print 9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,3 +5,32 @@
  */
 void times_table(void)
 {
+	int a;
+	int b;
+	int op;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar(48);
+
+		for (b = 1; b <= 9; b++)
+		{
+			op = a * b;
+			_putcha(44);
+			_putchar(32);
+
+			if (op <= 9)
+			{
+				_putchar(32);
+				_putchar(op + 48);
+			}
+			else
+			{
+				_putchar((op / 10) + 48);
+				_putchar((op % 10) +48);
+			}
+		}
+		_putchar('\n');
+	}
+}
+
