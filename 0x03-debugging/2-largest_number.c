a052162ae3c6653244dd5dcc87f07efe2396f887
GMDLULI <gugulethumdluli98@gmail.com>
2022-10-20 09:00:49 -0700

    print remaining days

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,21 +10,19 @@
 
 int largest_number(int a, int b, int c)
 {
-	if (a > b)
+	int largest;
+	
+	if (a > b && b > c)
 	{
-		if (b > c)
-			largest = a;
-		else if (a > c)
-			largest = a;
-		else
-			largest = c;
+		largest = a;
+	}
+	else if (b > a && a > c)
+	{
+		largest = b;
 	}
 	else
 	{
-		if (b < c)
-			largest = c;
-		else
-			largest = b;
+		largest = c;
 	}
 	return (largest);
 }
