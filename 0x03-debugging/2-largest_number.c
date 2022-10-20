a3f66c4ff5c2efaf80cd43593d0f4fbf42fac99e
GMDLULI <gugulethumdluli98@gmail.com>
2022-10-20 12:41:20 -0700

    largest number

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,27 +12,17 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b)
+	if ('a' >= 'b' && 'a' >= 'c')
 	{
-		if (b > c)
-		{
-			largest = a;
-		}
-		else if (a > c)
-		{
-			largest = a;
-		}
-		else
-			largest = c
+		largest = a;
 	}
-	if (b < c);
-	{
-		largest = c;
-	}
-	else
+	else if ('b' >= 'a' && 'b' >= 'c')
 	{
 		largest = b;
 	}
-	
+	else
+	{
+		largest = c;
+	}
 	return (largest);
 }
