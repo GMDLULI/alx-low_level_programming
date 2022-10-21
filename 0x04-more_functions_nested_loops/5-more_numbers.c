e6a88bd3e99da0697ea4b813f06f7eb4ec82bcbb
GMDLULI <gugulethumdluli98@gmail.com>
2022-10-21 14:53:27 -0700

    print more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,20 +8,17 @@
 
 void more_numbers(void)
 {
-	int i; 
+	int i , j;
 
-	while (i++ <= 10)
+	for (i = 0 i <= 10 i++)
 	{
-		int j;
 
-		for (j = 0; j <= 14; j++)
+		for (j = 0; j < 15; j++)
 		{
 			if (j >= 10)
-			{
-				_putchar((j / 10) + 0);
-				_putchar((j % 10) + 0);
-			}
-			_putchar('\n');
+				_putchar((j / 10) + 48);
+			_putchar((j % 10) + 48);
 		}
+		_putchar('\n');
 	}
 }
