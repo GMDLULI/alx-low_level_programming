57b995c337c8db21cd6be305e59afcb691acc5fb
GMDLULI <gugulethumdluli98@gmail.com>
2022-10-21 14:21:27 -0700

    print numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print_most_numbers- print numbers 0 to 9 execpt '2' and '4'
- *
- * Return: Always 0
+ * print_most_numbers- print numbers 0 to 9
+ * (neither 2 nor 4).
+ * Return: no return.
  */
 
 void print_most_numbers(void)
