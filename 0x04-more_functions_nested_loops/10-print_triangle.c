c120ac3a29a5eec524ffcc5c61293c8e450b8979
GMDLULI <gugulethumdluli98@gmail.com>
2022-10-22 00:57:37 -0700

    print triangle

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,23 +7,16 @@
  */
 void print_triangle(int size)
 {
-	int i;
-	int j;
+	int i, j;
 
 	for (i = 0; i < size; i++)
 	{
-		for (j = 1; j < (size - i); j++)
-		{
+		for (j = 1; j < (size - 1); j++)
 			_putchar(' ');
-		}
 		for (j--; j < size; j++)
-		{
 			_putchar(35);
-		}
 		if (i < (size - 1))
-		{
 			_putchar('\n');
-		}
-		_putchar('\n');
 	}
+		_putchar('\n');
 }
