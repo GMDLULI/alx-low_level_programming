845dfbd92d2a502f708c5dd3bcb5f8c3b9af8aef
GMDLULI <gugulethumdluli98@gmail.com>
2022-10-24 11:48:32 -0700

    print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts- print a string
+ * @str: string
+ * return: return string
+ */
+
+void _puts(char *str)
+{
+
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
+}
+
