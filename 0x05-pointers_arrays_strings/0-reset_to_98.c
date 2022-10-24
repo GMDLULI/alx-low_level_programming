bd5e3418dfdb1d7fd64a4361a8ed0fe6df9fdfe7
GMDLULI <gugulethumdluli98@gmail.com>
2022-10-24 10:29:04 -0700

    reset to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,21 +2,12 @@
 
 /**
  * reset_to_98- takes the pointer to an int and updates value it points
- * @n: interger
- * return: Always 0
+ * @n: input
+ * return: n
  */
 
 void reset_to_98(int *n)
 {
-	int p;
-
-	p = 98;
-	n  = &p;
-
-	_putchar(p);
-	_putchar('\n');
-	_putchar(&p);
-	_putchar('\n');
-	_putchar(n);
-	_putchar('\n');
+	*n = 98;
 }
+
