a323e49a6f8cb6633ceb19bdee3e2c93e79d00cc
GMDLULI <gugulethumdluli98@gmail.com>
2022-10-24 12:12:19 -0700

    print string in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -11,14 +11,10 @@ void print_rev(char *s)
 	int a = 0;
 
 	while (s[a] != '\0')
-	{
 		a++;
 
-		for (a = a - 1; a >= 0; a--)
-		{
-
+	for (a = a - 1; a >= 0; a--)
 		_putchar(s[a]);
-		}
-	}
+
 	_putchar('\n');
 }
