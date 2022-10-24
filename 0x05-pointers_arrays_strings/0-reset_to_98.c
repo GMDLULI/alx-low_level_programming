817e2530082c8aac6ac085081eee81c23efcb9a3
GMDLULI <gugulethumdluli98@gmail.com>
2022-10-24 10:00:16 -0700

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * reset_to_98- takes the pointer to an int and updates value it points
+ * @n: interger
+ * return: Always 0
+ */
+
+void reset_to_98(int *n)
+{
+	int p;
+
+	p = 9;
+	n  = &p;
+
+	_putchar(p);
+	_putchar('\n');
+	_putchar(*n);
+	_putchar('\n');
+}
