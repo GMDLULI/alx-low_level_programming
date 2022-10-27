0a17c3218cb58107d0d3ccf8d1ff29440edb1f98
GMDLULI <gugulethumdluli98@gmail.com>
2022-10-27 13:45:40 -0700

    compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,11 +11,14 @@ int _strcmp(char *s1, char *s2)
 {
 	int a, flag;
 
-	for (a = 0; s1[a] != '\0' || s2[a] != '\0'; a++)
+	a = 0
+	while (s1[a] != '\0' && s2[a] != '\0';)
 	{
 		if (s2[a] != s1[a])
+		{
 			return (s1[a] - s2[a]);
-			a++;
+		}
+	a++;
 	}
 	return (0);
 }
