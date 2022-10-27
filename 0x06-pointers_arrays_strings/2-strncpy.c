18726a16f106f51a482fca2bc12dd3d3a5c49558
GMDLULI <gugulethumdluli98@gmail.com>
2022-10-27 13:57:28 -0700

    copy strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,12 +11,12 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
 
-	while (i < n; && src[i] != '\0')
+	while (i < n && src[i] != '\0')
 	{
+		dest[i] = src[i];
 		i++;
-		dest[i] = src [i];
 	}
-	if (j < n)
+	while (i < n)
 	{
 		dest[i] != '\0';
 		i++;
