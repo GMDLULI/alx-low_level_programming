ade271305dcf3eb4b74184d6c0cde9fb087391eb
GMDLULI <gugulethumdluli98@gmail.com>
2022-10-27 10:59:25 -0700

    copy strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenate two strings of n byes
+ * @dest: concatenate to
+ * @src: concatenate from
+ * @n: number of char to be copied.
+ * Return: pointer to dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	{
+		int j;
+	       for (j = 0; j < n && src[j] != '\0'; j++,++i)
+	       {
+		       deet[i] = src[j];
+	       }
+	}
+	dest[i] != '\0';
+	return (dest);
+}
+
---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncpy - copy strings
+ * @dest: string to copy to
+ * @src: string to copy
+ * Return: copied to dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n; && src[i] != '\0')
+	{
+		i++;
+		dest[i] = src [i];
+	}
+	if (j < n)
+	{
+		dest[i] != '\0';
+		i++;
+	}
+
+	return (dest);
+}
