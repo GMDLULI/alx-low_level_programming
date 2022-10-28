3279bb91e0be542329bc35e76bb4a8b5fc534c11
GMDLULI <gugulethumdluli98@gmail.com>
2022-10-28 10:20:01 -0700

    concatenate

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,17 +11,22 @@
 char *_strncat(char *dest, char *src, int n)
 {
 	int i;
+	int j;
 
-	for (i = 0; dest[i] != '\0'; i++)
+	i = 0;
+	while (dest[i] != '\0')
 	{
-		int j;
-
-		for (j = 0; j < n && src[j] != '\0'; j++, ++i)
-		{
-			dest[i] = src[j];
-		}
+		i++;
 	}
-	dest[i] != '\0';
+
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+
 	return (dest);
 }
-
