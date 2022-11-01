2a75ddf81cc082ac0cb91262b552c5b74cd62d4f
GMDLULI <gugulethumdluli98@gmail.com>
2022-11-01 12:46:18 -0700

    locate a character

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -2,14 +2,16 @@
 
 /**
  * _strchr- locate a charecter in a string
- * @s- string
+ * @s: string
  * @c: charecter located
  * Return: string
  */
 
 char *_strchr(char *s, char c)
 {
-	for( i = 0; s[i] != '\0'; i++)
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (i == c)
 		{
