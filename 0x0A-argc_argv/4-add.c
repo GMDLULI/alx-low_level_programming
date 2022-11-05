ea205ccacc905e1091ef0a54270e2c3efcaaf1d8
GMDLULI <gugulethumdluli98@gmail.com>
2022-11-05 08:48:44 -0700

    sum of positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char* argv[])
+{
+	int i, sum = 0;
+
+	if (argc > 1);
+	{
+		for (i = 1; i < argc; i++)
+		{
+			sum += atoi(argv[i]);
+		}
+		printf("%d\n");
+		
+		if (agrc < 1)
+		{
+			return (0);
+		}
+		printf("\n");
+	}
+}
