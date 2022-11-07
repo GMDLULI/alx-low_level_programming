d0140051b9967fa3389964cae04254351881a561
GMDLULI <gugulethumdluli98@gmail.com>
2022-11-07 05:58:22 -0800

    print number of arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -7,9 +7,9 @@
  * Return: 0
  */
 
-int main(int argc, char **argv _attribute_((unused)));
+int main(int argc, char __attribute__((__unused__)) *argv[])
 {
-	printf("%d\n", (argc - 1));
+	printf("%d\n", argc - 1);
 
 	return (0);
 }
