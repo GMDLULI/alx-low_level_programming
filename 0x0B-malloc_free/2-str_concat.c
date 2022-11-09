69b4909007fb7fd61a3a52fc259794efb1eb004b
GMDLULI <gugulethumdluli98@gmail.com>
2022-11-09 15:50:57 -0800

    concatenate strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,7 +19,7 @@ int _strlen(char *s)
 
 
 /**
- * str_concate- concatenate two strings
+ * str_concat- concatenate two strings
  * @s1: first string
  * @s2: second string
  * Return: a pointer
@@ -36,17 +36,17 @@ char *str_concat(char *s1, char *s2)
 	concate = malloc(sizeof(char) * (j + 1));
 
 	if (s1 == NULL)
-		s1 = "";
+		s1 = " ";
 
 	if (s2 == NULL)
-		s2 = "";
+		s2 = " ";
 
 	if (concate == NULL)
 		return (NULL);
 
 	for (i = 0; i < _strlen(s1); i++)
 
-		concate[i] = s2[i];
+		concate[i] = s1[i];
 
 	for (i = 0; i < _strlen(s2); i++)
 
