21ce529c3605105f607f50c79733eb6af606e63c
GMDLULI <gugulethumdluli98@gmail.com>
2022-11-09 15:39:20 -0800

    concatenate strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,7 +1,25 @@
 #include "main.h"
 
 /**
- * _concate- concatenate two strings
+ * _strlen- calculates the length of string
+ * @s: string
+ * Return: the length of string
+ */
+
+int _strlen(char *s)
+{
+	int len, i;
+
+	len = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+		len++;
+	return (len);
+}
+
+
+/**
+ * str_concate- concatenate two strings
  * @s1: first string
  * @s2: second string
  * Return: a pointer
@@ -9,24 +27,39 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	int i = 0;
-	int j;
-	int n;
+	int i, j;
 
-	char *str = (char*)melloc(n * sizeof(char));
+	char *concate;
 
-	while (s1[i] != '\0')
-	{
-		i++;
-	}
-	while (j < n; s2[j] != '\0')
-	{
-		i++;
-		j++;
-	}
-	s1[i] = '\0';
-	return(s1);
-	
+	j = _strlen(s1) + _strlen(s2);
+
+	concate = malloc(sizeof(char) * (j + 1));
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	if (concate == NULL)
+		return (NULL);
+
+	for (i = 0; i < _strlen(s1); i++)
+
+		concate[i] = s2[i];
+
+	for (i = 0; i < _strlen(s2); i++)
+
+		concate[_strlen(s1) + i] = s2[i];
+	return (concate);
+}
+
+
+
+
+
+
+
 }
 
 
