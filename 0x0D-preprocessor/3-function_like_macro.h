a6d2c533b09bd55a62478b1639a641837d90242f
GMDLULI <gugulethumdluli98@gmail.com>
2022-11-15 03:35:35 +0000

    function like macro

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#define ABS(x) x
+
+int main ()
+{
+#if ABS(X) < 0
+	printf("%d\n", x);
+#else
+	printf("%d\n", x);
+#endif
+}
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,8 @@
+#include <stdio.h> 
+#define sum (x,y) ((x) + (y))
+
+int main ()
+{
+	int n = sum (x,y);
+	printf("%d\n", n);
+}
