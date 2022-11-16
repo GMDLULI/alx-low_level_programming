017793383b03282c0cacd7a5fbde0884bcfbddbb
GMDLULI <gugulethumdluli98@gmail.com>
2022-11-16 04:54:15 +0000

    initialize struct

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -6,13 +6,12 @@
  * @name: name of dog
  * @age: dogs age
  * @owner: the name of the owner of the dog
-
  * Return: void
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d != NULL)
+	if (d)
 	{
 		d->name = name;
 		d->age = age;
