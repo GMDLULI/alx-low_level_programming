6908c2eb18bc7621175b1078ced35d75fc6cf096
GMDLULI <gugulethumdluli98@gmail.com>
2022-11-18 03:26:55 +0000

    print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * print_name- prints a name
+  * @f: function pointer
+  * @name: the name to be printed
+  * Return: no return
+  */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		
+		f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,2 @@
+#!bin/bash 
+working with function pointers, project 0x0F-function pointers
