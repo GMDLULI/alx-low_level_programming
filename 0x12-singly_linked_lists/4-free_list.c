5d518fee2b004cb85375469cc6e3ffdf5754ade8
GMDLULI <gugulethumdluli98@gmail.com>
2022-11-26 07:48:30 +0000

    free

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+  * free_list - frees a list_t list
+  * @head: a pointer to the list_t list
+  */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
+
