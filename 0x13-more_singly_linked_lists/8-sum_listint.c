9de1d2085737b822bce75312eb6acc929aab6d97
GMDLULI <gugulethumdluli98@gmail.com>
2022-11-30 05:06:30 +0000

    sum of list

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -3,18 +3,18 @@
 /**
   * sum_listint - returns the sum of linked list
   * @head: pointer to the first linked list
-  * Return: sum
+  * Return: if the list is empty - 0.
+  *        otherwise - the sum of all data
   */
 
 int sum_listint(listint_t *head)
 {
-	int sum;
-	listint_t *ptr = head;
+	int sum = 0;
 
-	while (ptr != NULL)
+	while (head)
 	{
-		sum += ptr->n;
-		ptr = ptr->next;
+		sum += head->n;
+		head = head->next;
 	}
 	return (sum);
 }
