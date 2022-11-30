585af4323398bd2ae07faa834223c30182484ae1
GMDLULI <gugulethumdluli98@gmail.com>
2022-11-30 04:06:17 +0000

    add node at end

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -13,14 +13,24 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 
 	ptr = *head;
 	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+
 
 	new->n = n;
 	new->next = NULL;
 
-	while (ptr->next != NULL)
+	if (*head == NULL)
+		*head = new;
+
+	else
 	{
-		ptr = ptr->next;
+		while (ptr->next != NULL)
+		{
+			ptr = ptr->next;
+		}
+		ptr->next = new;
 	}
-	ptr->next = new;
-	return (new);
+
+	return (*head);
 }
