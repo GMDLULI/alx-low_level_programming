79a090e7f78ed25da47e8ea628894b69a8210778
GMDLULI <gugulethumdluli98@gmail.com>
2022-12-01 12:28:09 +0000

    print elements of list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,20 +10,17 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t nelem;
+	size_t count;
 
-	nelem = 0;
+	count = 0;
 
 	while (h != NULL)
 	{
-		if (h->str == NULL)
 
-			printf("[%d] %s\n",0 ,"(nil)");
-		else
-			printf("[%d} %s\n", h->len, h->str);
+		printf("[%d} %s\n", h->len, h->str);
 		h = h->next;
-		nelem++;
+		count++;
 	}
-	return (nelem);
+	return (count);
 }
 
