d5a35161bdce3d823dd2db7c95da4bec082eec83
GMDLULI <gugulethumdluli98@gmail.com>
2022-12-03 02:30:47 +0000

    print binary

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,6 +1,10 @@
 #ifdef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <string.h>
+#include <stdarg.h>
+
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int _putchar(char c);
