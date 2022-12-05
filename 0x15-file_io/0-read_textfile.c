248676ecd2dff9220afe0c4bb49c31f8559d6eed
GMDLULI <gugulethumdluli98@gmail.com>
2022-12-05 23:52:30 +0000

    read and print file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+  * read_textfile - reads and prints a file
+  * @filename: pointer to file name
+  * @letters: number of letters of read
+  * Return: file contant
+  */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	size_t fb, i, w;
+	char *buff;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+
+	buff = malloc(sizeof(char) * letters);
+	if (buff == NULL)
+	{
+		return (0);
+	}
+
+	fb = open(filename, O_RDONLY);
+	i = read(fb, buff, letters);
+	w = write(STDOUT_FILENO, buff, i);
+
+	if (fb == -1 || i == -1 || w == -1 | w != i)
+	{
+		free(buff);
+		return (0);
+	}
+
+	free(buff);
+	close(fb);
+
+	return (w);
+}
+
+
