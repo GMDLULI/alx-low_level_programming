1b2d9db4ca84c6b35eab19d65702d785d72c89c5
GMDLULI <gugulethumdluli98@gmail.com>
2022-12-06 03:02:30 +0000

    create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (text_content != NULL)
 	{
-		for (i = 0; text_content[i] != '\0'; i++)
+		for (i = 0; text_content[i];)
 			i++;
 	}
 
